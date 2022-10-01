1d02aee8f1dd5e752117079e5b7c5dea1c8b661a
Jose Barreto <18638509+Jose-Barreto@users.noreply.github.com>
2022-10-01 14:31:31 -0700

    Updated Get-CommandExtra
    Added messages explaining what the cmdlet does
    Added variables for module name and output file name

--- Get-CommandExtra.ps1
+++ Get-CommandExtra.ps1
@@ -1,5 +1,11 @@
+$Module = "Azure"
+$Output = ".\" + $Module + "Cmdlets.HTML"
+
+"Create HTML file with all commands and parameters for the " + $Module + " Module"
+"Output goes to the " + $Output + "File"
+
 $Cmdlet = "" | Select Name, Parameters
-Get-command -Module Azure | % {
+Get-command -Module $Module | % {
   Get-Help $_ | % { 
     $ParameterSetCount = 1
     $CmdletName = $_.Name
@@ -7,30 +13,30 @@ Get-command -Module Azure | % {
     $ParameterSets | % {
 
       If ($ParameterSets.Count -gt 1) { 
-        $Cmdlet.Name=$CmdletName+” (“+$ParameterSetCount+”)”
+        $Cmdlet.Name=$CmdletName+" ("+$ParameterSetCount+")"
         $ParameterSetCount++
       } else { 
         $Cmdlet.Name=$CmdletName 
       } 
 
       $Parameters = $_.parameter
-      $StringPar=””
+      $StringPar=""
       If ($Parameters) {
         $First=$true
         $Parameters | % { 
           If ($First) {$First=$false} else {$StringPar+="%%"}
-          $StringPar+= “ -“+$_.name+” “; 
-          if ($_.parameterValue -and ($_.parameterValue –notlike “Switch*”)) { 
-            $StringPar+= ”<”+$_.parameterValue+”> “ 
+          $StringPar+= " -"+$_.name+" "; 
+          if ($_.parameterValue -and ($_.parameterValue -notlike "Switch*")) { 
+            $StringPar+= "<"+$_.parameterValue+"> " 
           } 
         }
       }
-      $StringPar=$StringPar.Replace(“Nullable``1”,””)
-      $StringPar=$StringPar.Replace(“System.[[System.Int32, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]”,”Int32”)
-      $StringPar=$StringPar.Replace(“System.[[System.Boolean, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]”,”Boolean”)
+      $StringPar=$StringPar.Replace("Nullable``1","")
+      $StringPar=$StringPar.Replace("System.[[System.Int32, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]","Int32")
+      $StringPar=$StringPar.Replace("System.[[System.Boolean, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]","Boolean")
       $Cmdlet.parameters=$StringPar;
 
       $Cmdlet  
     }
   } 
-} | ConvertTo-HTML | Out-File D:\AzureCmdlets.HTML
+} | ConvertTo-HTML | Out-File $Output
