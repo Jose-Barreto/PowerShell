9e41202b96022a679b6834452217f5166ed2fa35
Jose Barreto <18638509+Jose-Barreto@users.noreply.github.com>
2022-10-02 09:35:16 -0700

    Document Module Parameters
    Added version of this cmdlet to show a list of cmdlets and parameters for SharePoint.
    Outputs a list instead of an HTML table.

---
+++ DocumentModuleParameters.ps1
@@ -0,0 +1,41 @@
+$Module = "Microsoft.Online.SharePoint.PowerShell"
+$Output = ".\" + $Module + "Cmdlets.HTML"
+
+"Create HTML file with all commands and parameters for the " + $Module + " Module"
+"Output goes to the " + $Output + "File"
+
+$Cmdlet = "" | Select Name, Parameters
+Get-command -Module $Module | % {
+  Get-Help $_ | % { 
+    $ParameterSetCount = 1
+    $CmdletName = $_.Name
+    $ParameterSets = $_.syntax.syntaxItem 
+    $ParameterSets | % {
+
+      If ($ParameterSets.Count -gt 1) { 
+        $Cmdlet.Name=$CmdletName+" ("+$ParameterSetCount+")"
+        $ParameterSetCount++
+      } else { 
+        $Cmdlet.Name=$CmdletName 
+      } 
+
+      $Parameters = $_.parameter
+      $StringPar=""
+      If ($Parameters) {
+        $First=$true
+        $Parameters | % { 
+          If ($First) {$First=$false} else {$StringPar+=" "}
+          $StringPar+= " -"+$_.name+" "; 
+          if ($_.parameterValue -and ($_.parameterValue -notlike "Switch*")) { 
+            $StringPar+= "&lt;"+$_.parameterValue+"&gt; " 
+          } 
+        }
+      }
+      $StringPar=$StringPar.Replace("Nullable``1","")
+      $StringPar=$StringPar.Replace("System.[[System.Int32, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]","Int32")
+      $StringPar=$StringPar.Replace("System.[[System.Boolean, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]","Boolean")
+      $Cmdlet.parameters=$StringPar;
+      "<p><b>"+$Cmdlet.Name+"</b> " + $Cmdlet.Parameters+"</p>"
+    }
+  } 
+} | Out-File $Output
